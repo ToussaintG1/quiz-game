719e41ec1ce9e19a270c35b2015a50222c48d8dc
Toussaint Gilbert <toussaintdgilbert@gmail.com>
2023-10-21 20:32:58 -0500

    index.html edits, new styles in style.css, fixed error with start-text displaying randomly

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,11 @@ const questions = [
         id: 0,
         // Commonly used data types DO NOT include : Booleans
         question: "Commonly used data types DO NOT include:",
-        answers: [
-            {text: "Strings", correct: false},
-            {text: "Booleans", correct: true},
-            {text: "Alerts", correct: false},
-            {text: "Numbers", correct: false},
+        answer: [
+            {text: "Strings", isCorrect: false},
+            {text: "Booleans", isCorrect: true},
+            {text: "Alerts", isCorrect: false},
+            {text: "Numbers", isCorrect: false},
         ]
 
     },
@@ -16,66 +16,75 @@ const questions = [
         id: 1,
         // The condition in an if/else statement is enclosed within ________. : Parenthesis
         question: "The condition in an if/else statement is enclosed within ________.",
-        answers: [
-            {text: "Quotes", correct: false},
-            {text: "Square brackets", correct: false},
-            {text: "Curly brackets", correct: false},
-            {text: "Parenthesis", correct: true},
+        answer: [
+            {text: "Quotes", isCorrect: false},
+            {text: "Square brackets", isCorrect: false},
+            {text: "Curly brackets", isCorrect: false},
+            {text: "Parenthesis", isCorrect: true},
         ]
     },
     {
         id: 2,
         // Arrays in JavaScript can be used to store ________. : All of the above
         question: "Arrays in JavaScript can be used to store ________.",
-        answers: [
-            {text: "Numbers & Strings", correct: false},
-            {text: "Other arrays", correct: false},
-            {text: "booleans", correct: false},
-            {text: "All of the above", correct: true},
+        answer: [
+            {text: "Numbers & Strings", isCorrect: false},
+            {text: "Other arrays", isCorrect: false},
+            {text: "booleans", isCorrect: false},
+            {text: "All of the above", isCorrect: true},
         ]
     },
     {
         id: 3,
        // String values must be enclosed within ________ when being assigned to variables. : Quotes
         question: "String values must be enclosed within ________ when being assigned to variables.",
-        answers: [
-            {text: "Commas", correct: false},
-            {text: "Curly brackets", correct: false},
-            {text: "Quotes", correct: true},
-            {text: "Parenthesis", correct: false},
+        answer: [
+            {text: "Commas", isCorrect: false},
+            {text: "Curly brackets", isCorrect: false},
+            {text: "Quotes", isCorrect: true},
+            {text: "Parenthesis", isCorrect: false},
         ]
     },
     {
         id: 4,
      // A very useful tool used during the development and debugging for printing content to the debugger is: Console log
     question: "A very useful tool used during the development and debugging for printing content to the debugger is:",
-    answers: [
-        {text: "JavaScript", correct: false},
-        {text: "Terminal/bash", correct: false},
-        {text: "For loops", correct: false},
-        {text: "Console log", correct: true},
+    answer: [
+        {text: "JavaScript", isCorrect: false},
+        {text: "Terminal/bash", isCorrect: false},
+        {text: "For loops", isCorrect: false},
+        {text: "Console log", isCorrect: true},
     ]
 },
 {
     id: 5,
     // How do you write 'Hello World' in an alert box? : alert('Hello World')
     question: "How do you write 'Hello World' in an alert box",
-    answers: [
-        {text: "alertBox('Hello World')", correct: false},
-        {text: "alert('Hello World')", correct: true},
-        {text: "msg('Hello World')", correct: false},
-        {text: "msgBox('Hello World", correct: false},
+    answer: [
+        {text: "alertBox('Hello World')", isCorrect: false},
+        {text: "alert('Hello World')", isCorrect: true},
+        {text: "msg('Hello World')", isCorrect: false},
+        {text: "msgBox('Hello World", isCorrect: false},
     ]
 },
 {
     id: 6,
    // How do you create a function in JavaScript : function = myFunction()
    question: "How do you create a function in JavaScript",
-    answers: [
-        {text: "function myFunction(){}", correct: true},
-        {text: "function = myFunction()", correct: false},
-        {text: "function: myFunction()", correct: false},
-        {text: "function[]", correct: false}
+    answer: [
+        {text: "function myFunction(){}", isCorrect: true},
+        {text: "function = myFunction()", isCorrect: false},
+        {text: "function: myFunction()", isCorrect: false},
+        {text: "function[]", isCorrect: false}
+    ]
+},
+{
+    id: 7,
+    question: "END OF QUIZ",
+    answer: [{ text: "Thank You", isCorrect: true },
+    { text: "For Taking", isCorrect: true },
+    { text: "My", isCorrect: true },
+    { text: "Coding Quiz", isCorrect: true }
     ]
 }
     ];
@@ -85,7 +94,7 @@ const questions = [
 
     //Main Variables
 var timer = document.getElementById("timer");
-var start = document.querySelector(".start-button");
+var start = document.querySelector("#quizStartButton");
 var quiz = document.getElementById("quiz");
 var timeLeft = 90;
 
@@ -95,6 +104,11 @@ var question = document.getElementById("question");
 var id = 0;
 
 var answers = document.querySelectorAll(".answer")
+var answer1 = document.getElementById("answer1");
+var answer2 = document.getElementById("answer2");
+var answer3 = document.getElementById("answer3");
+var answer4 = document.getElementById("answer4");
+var results = document.getElementById("result");
 
 // Highscore board at end of quiz
 var scoreWrapper = document.getElementById("score");
@@ -108,53 +122,16 @@ var scoreList = document.getElementById("score-list");
 var userScores = [];
 
 // Button Variables
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn")
 var viewHighscoresBtn = document.querySelector(".highscore");
 var tryAgain = document.querySelector(".try-again");
-var startBtn = document.getElementById("quizStartButton");
-var returnBtn = document.getElementById("returnBtn");
-var clearBtn = document.getElementById("clearBtn");
+var startButton = document.getElementById("quizStartButton");
+var returnBtn = document.getElementById("returnButton");
+var clearBtn = document.getElementById("clearButton");
 var returnStrtBtn = document.querySelector(".return-start");
 
 
-//Start questions and score at 0
-let currentQuestionIndex = 0;
-let score = 0;
-
 // -----------Functions---------
-//Start Quiz
-function startQuiz() {
-    currentQuestionIndex = 0;
-    score = 0;
-    nextButton.innerHTML = "Next";
-    showQuestion();
-    console.log("Quiz Started");
-    countDown();
-    subtractTime();
-};
 
-
-//Shows the questions and answer buttons' text
-function showQuestion() {
-    resetState();
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
-
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.classList.add("btn");
-        answerButtons.appendChild(button);
-        //If answer is correct in the Java text this will make it correct.
-        if(answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer);
-        
-    });
-};
 
 //Remove all previous answers and replace with JavaScript answers.
 function resetState() {
@@ -164,29 +141,6 @@ function resetState() {
     }
 }
 
-//When user clicks on a button the computer will check if the dataset is true.
-function selectAnswer(e) {
-    const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
-    //If true, add the class name "correct" and increase score
-    if(isCorrect){
-        selectedBtn.classList.add("correct");
-        score++;
-        //If NOT true, add the class name "incorrect"
-    } else {
-        selectedBtn.classList.add("incorrect")
-    }
-    
-    //For each button, check data set. If true, add class "correct" to highlight
-    //the correct answer, then disable the buttons and display the "Next" button.
-    Array.from(answerButtons.children).forEach(button => {
-        if(button.dataset.correct === "true") {
-            button.classList.add("correct");
-        }
-        button.disabled = true;
-    });
-    nextButton.style.display = "block";
-};
 
 //Display the score out of the length of questions 
 function showScore(){
@@ -196,15 +150,6 @@ function showScore(){
     nextButton.style.display = "block";
 }
 //After answering a question it will take you to the next question
-//If there are no more questions, the game is over and score is shown.
-function nextQuestion() {
-    currentQuestionIndex++;
-    if(currentQuestionIndex < questions.length){
-        showQuestion();
-    } else {
-        showScore();
-    }
-}
 
 function showUserScores() {
     scoreList.innerHTML = "";
@@ -234,41 +179,206 @@ function storeScore() {
     localStorage.setItem("userScores", JSON.stringify(userScores));
 };
 
-//When clicking on the "Next" button, it will take you to the next question.
-nextButton.addEventListener("click", ()=> {
-    if(currentQuestionIndex < questions.length){
-        nextQuestion();
-    } else {
-        startQuiz();
-    }
-})
-
-//When Clicking Start Quiz button the quick will start
-quizBtn.addEventListener('click', startQuiz);
 
 //Timer Code, if time runs out. Show score and Game is over
+function countdown() {
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        timer.textContent = timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(timeInterval);
+            timer.textContent = "0";
+            quiz.style.display = "none";
+            scoreWrapper.style.display = "flex";
+            scoreText.innerText = "Sorry you ran out of time, A score of zero cannot be saved. Better luck next time!";
+            form.style.display = "none";
+            returnStrtBtn.style.display = "flex";
+            tryAgain.style.display = "flex";
+        } else if (id === 7 && scoreWrapper.style.display === "none") {
+            clearInterval(timeInterval);
+            timer.textContent = timeLeft;
+            quiz.style.display = "none"
+            scoreWrapper.style.display = "flex";
+            tryAgain.style.display = "none";
+            returnStrtBtn.style.display = "none";
+            form.style.display = "flex";
+            scoreText.innerText = "Congratulations your score is: " + timeLeft;
+        };
 
-countDown(60,"timer-sec");
-function countDown(secs,elem) {
-    const timeLeftEl = document.getElementById(elem);
-    timeLeftEl.innerHTML = secs;
-        secs--;
-        var timer = setTimeout('countDown('+secs+',"'+elem+'")', 1000);
-        if(secs < 0){
-            clearTimeout(timer);
-            alert('Time is up!')
-            showScore()
-        }  
-}
+    }, 1000);
+};
 
-//If Incorrect subtract 5 seconds.
-function subtractTime() {
-    while(!isCorrect){
-        secs -= 10;
+// Function to cycle through the questions
+function questionCycle(id) {
+    var startText = document.getElementById("start-text");
+    startText.style.display = "none";
+    // Add the text we want to display from our Questions array
+    question.innerText = questions[id].question;
+
+    // Add the text we want to display from our Questions array
+    answer1.innerText = questions[id].answer[0].text;
+    answer2.innerText = questions[id].answer[1].text;
+    answer3.innerText = questions[id].answer[2].text;
+    answer4.innerText = questions[id].answer[3].text;
+
+    // Add true or false using isCorrect boolean
+    answer1.value = questions[id].answer[0].isCorrect;
+    answer2.value = questions[id].answer[1].isCorrect;
+    answer3.value = questions[id].answer[2].isCorrect;
+    answer4.value = questions[id].answer[3].isCorrect;
+
+    if (id === 7) {
+        answer1.disabled = true;
+        answer2.disabled = true;
+        answer3.disabled = true;
+        answer4.disabled = true;
+    } else {
+        answer1.disabled = false;
+        answer2.disabled = false;
+        answer3.disabled = false;
+        answer4.disabled = false;
+    };
+};
+
+// Clear Score Board
+function reloadScoreboard() {
+    while (scoreList.hasChildNodes()) {
+        scoreList.removeChild(scoreList.firstChild);
+    };
+    clearScores();
+};
+
+// Submit button 
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var initalsText = userInitials.value.trim().toUpperCase() + ":" + " " + timeLeft;
+    if (initalsText === "") {
+        return;
+    };
+
+    if (scoreBoard.style.display === "none") {
+        scoreBoard.style.display = "flex";
+        scoreWrapper.style.display = "none";
+    } else {
+        scoreBoard.style.display = "none";
+        scoreWrapper.style.display = "flex";
+    };
+
+    if (viewHighscoresBtn.disabled = true) {
+        viewHighscoresBtn.disabled = false;
+    };
+
+    userScores.push(initalsText);
+
+    storeScore();
+    loadUserScores();
+});
+
+// Start Button
+startButton.addEventListener("click", function () {
+
+    if (quiz.style.display === "none" && id <= 9) {
+        quiz.style.display = "flex";
+    } else {
+        quiz.style.display = "none";
+    };
+
+    if (scoreWrapper.style.display === "flex") {
+        scoreWrapper.style.display = "none";
+    };
+    viewHighscoresBtn.disabled = true;
+    start.setAttribute("style", "display: none;");
+    quiz.setAttribute("style", "display: flex;");
+    id = 0;
+    countdown();
+    questionCycle(id);
+
+});
+
+// Answer button
+answers.forEach(answer => {
+    answer.addEventListener("click", function () {
+        if (answer.value == "true" && id <= 10) {
+            id++;
+            questionCycle(id);
+            result.innerText = "True!";
+            result.style.color = "green";
+        } else if (answer.value == "false") {
+            timeLeft -= 10;
+            result.innerText = "False!";
+            result.style.color = "red";
+            id++;
+            questionCycle(id);
+        };
+
+    });
+});
+
+// Try Again Button
+tryAgain.addEventListener("click", function () {
+    timeLeft = 90;
+    id = 0;
+    countdown();
+    questionCycle(id);
+    quiz.style.display = "flex";
+    scoreWrapper.style.display = "none";
+});
+// View Highscores button
+viewHighscoresBtn.addEventListener("click", function () {
+    if (scoreBoard.style.display === "none") {
+        scoreBoard.style.display = "flex";
+        start.style.display = "none";
+    };
+});
+
+// Return button
+returnBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (scoreBoard.style.display === "flex") {
+        scoreBoard.style.display = "none";
+        scoreWrapper.style.display = "none";
+        start.style.display = "flex";
+        timeLeft = 90;
+    };
+});
+
+// Return to start button
+returnStrtBtn.addEventListener("click", function () {
+    if (scoreWrapper.style.display === "flex") {
+        scoreWrapper.style.display = "none";
+        start.style.display = "flex";
+        viewHighscoresBtn.disabled = false;
+        timeLeft = 90;
+        questionCycle(id);
+    };
+});
+
+// Add user scores
+function addUserScores() {
+    scoreList.innerHTML = "";
+
+    for (var i = 0; i < userScores.length; i++) {
+        var score = userScores[i];
+
+        var li = document.createElement("li");
+        li.textContent = score;
+        li.setAttribute("data-index", i);
+
+        scoreList.appendChild(li);
     }
-}
-    
-   
+};
 
-startQuiz();
+// Save user scores
+function storeScore() {
+    localStorage.setItem("userScores", JSON.stringify(userScores));
+};  
+
+function clearScores() {
+    localStorage.clear()
+    userScores = [];
+};
+   
+// Load Scores Once Done
+loadScores();
 
